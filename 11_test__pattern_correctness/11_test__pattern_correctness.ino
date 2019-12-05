c3f27e2154ca5ce194ef2fb00dc03596f90d5255
Mike Heavers <mheavers@gmail.com>
2019-12-05 10:25:33 -0800

    color cycling and pattern repetition working
    need to start back at first color after each submitted color in the pattern
    need to do something to signal a correct or incorrect guess
    need to do something to signal pattern display is complete and user should start bouncing

--- 11_test__pattern_correctness/11_test__pattern_correctness.ino
+++ 11_test__pattern_correctness/11_test__pattern_correctness.ino
@@ -2,39 +2,43 @@
 #include <Adafruit_NeoPixel.h>
 
 #define PIN 7 // Which pin on the Arduino is connected to the NeoPixels?
-#define NUMPIXELS 9
-Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-int ledBrightness = 30; //0 - 255
+#define NUMPIXELS 9 //how many lights are on our LED strip?
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800); //initialize neopixels
+
+int ledBrightness = 30; //0 - 255 - set the overall brightness of our strip
 float vibrationThreshold = 4; //above this value, trigger a bounce
 
-uint32_t colorRed = pixels.Color(255,0,0);
+//define the colors we want to cycle through
+uint32_t colorRed = pixels.Color(255,0,0); 
 uint32_t colorGreen = pixels.Color(0,150,0);
 uint32_t colorBlue = pixels.Color(0,255,255);
 uint32_t colorYellow = pixels.Color(255,255,0);
 
+//add them to an array in the order we want them to display
 uint32_t colorArray[4] = {colorRed,colorGreen,colorBlue,colorYellow};
 
-int curColorIndex = 0;
+int curColorIndex = 0; //keeps track of the current color to display
 
 //PATTERN
 
 bool isPatternMode = true; //when true, display patterns, when false, user repeats pattern
-int patternColorDisplayTime = 3000; //how long to display each color
+int patternColorDisplayTime = 3000; //how long to display each color in pattern mode
+int patternCompleteReadyTime = 1000; //how long to give the user after the pattern has been displayed before they can start jumping
 int curStage = 0; //level of the game (array index of patternArray colors to iterate through)
-uint32_t patternArray[4] = {colorRed,colorGreen,colorBlue,colorYellow};
+uint32_t patternArray[4] = {colorRed,colorGreen,colorBlue,colorYellow}; //TODO: randomize this pattern! //array specifying pattern which user will have to replicate. After reaching last color, they win the game 
 
 
 //PIEZO
-const int PIEZO_PIN = A0; // Piezo output
+const int PIEZO_PIN = A0; // pin on which we read vibration / piezo output
 
 //BOUNCE MODE
-int bounceTime = 1000; //how much time pass during vibration to constitute a bounce / cycle through colors
-int stopBounceTime = 5000; //how much time must pass after bounce to consider it a "submission" of the current color
-unsigned long time_now = 0;
-boolean isBounced = false;
-boolean hasBegunBouncing = false;
+int bounceTime = 1000; //how much time must during vibration to constitute a bounce / cycle through colors. Essentially this would be long enough to allow the trampoline to vibrate during a jump, but short enough that it's ready to detect again before the user lands, i.e. "airborne time"
+int stopBounceTime = 5000; //how much time must pass after bounce to consider it a "submission" of the current color - i.e. how long the user must wait off of the trampoline
+unsigned long time_now = 0; //keeps track of the passing of time to constitute a bounce and stop bounce
+boolean isBounced = false; //the user has started a bounce (should occur while each jump is occuring)
+boolean hasBegunBouncing = false; //true when user has triggered vibration for the first time for each color guess
 
-int correctCount = 0; //keeps track of how many colors have been guessed correctly so far for the current stage
+int correctCount = 0; //keeps track of how many colors have been guessed correctly so far for the current stage. Set back to zero each stage / level
 
 void setup() {
   Serial.begin(115200);
@@ -54,20 +58,20 @@ void loop() {
 
 }
 
-void handlePatternMode(){
+void handlePatternMode(){ //display the pattern the user must replicate
   for (int i = 0; i <= curStage; i++) {
     uint32_t curPatternColor = patternArray[i];
     for (int j = 0; j < NUMPIXELS; j++) { // For each pixel...
-      // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
+
       pixels.setPixelColor(j, curPatternColor);
-      pixels.show();   // Send the updated pixel colors to the hardware.
+      pixels.show(); //Send the updated pixel colors to the hardware.
     }
     delay(patternColorDisplayTime);
   }
 
   pixels.clear();
   pixels.show();
-  delay(1000);
+  delay(patternCompleteReadyTime);
   time_now = millis();
   isPatternMode = false;
 }
@@ -75,7 +79,7 @@ void handlePatternMode(){
 void handleBounceMode(){
   //PIEZO
   int piezoADC = analogRead(PIEZO_PIN);
-  float piezoV = piezoADC / 1023.0 * 5.0;
+  float piezoV = piezoADC / 1023.0 * 5.0; //scale the vibration to a more user friendly range
   
   /*
   if (piezoV > vibrationThreshold){
@@ -83,35 +87,38 @@ void handleBounceMode(){
   }
   */
 
-  if (piezoV > vibrationThreshold && !isBounced) {
+  if (piezoV > vibrationThreshold && !isBounced) { //if the vibration is significant enough to constitute a bounce, and we haven't already triggered a bounce...
     Serial.println("bounce");
-    for (int i = 0; i < NUMPIXELS; i++) { // For each pixel...
-      // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
+
+    time_now = millis();
+    
+    for (int i = 0; i < NUMPIXELS; i++) { // For each pixel on the strip
       uint32_t curPixelColor = colorArray[curColorIndex];
       pixels.setPixelColor(i, curPixelColor);
       pixels.show();   // Send the updated pixel colors to the hardware.
     }
-    isBounced = true; //register a bounce
+    isBounced = true; //register a bounce - keeps this block of code (pixel showing) from happening repeatedly
   }
 
-  if ((millis() > time_now + bounceTime) && isBounced && (millis() < time_now + stopBounceTime)) { //we have a completed bounce, but not a stop bouncing
+  if ((millis() > time_now + bounceTime) && isBounced && (millis() < time_now + stopBounceTime)) { //we have a completed bounce, but not a stop bouncing, i.e. user is airborne
+    isBounced = false; //keeps this block from repeating
     if (!hasBegunBouncing){
       Serial.println("has begun bouncing");
-      hasBegunBouncing = true;
+      hasBegunBouncing = true; //stays true while user is cycling through colors but has not yet submitted a guess
     }
     Serial.println("bounce");
     time_now = millis();
     pixels.clear();
     pixels.show();
-    if (curColorIndex < 3){
+    if (curColorIndex < 3){ //prepare the next color in the sequence to be displayed
       curColorIndex++;
     } else {
       curColorIndex = 0;
     }
-    isBounced = false;
-  } else if ((millis() >= time_now + stopBounceTime) && isBounced && hasBegunBouncing) { //we're pausing to see if our submission is correct
+    
+  } else if ((millis() >= time_now + stopBounceTime) && !isBounced && hasBegunBouncing) { //we've stopped jumping and are waiting to see if our submission is correct
     Serial.println("submit color guess");
-    hasBegunBouncing = false;
+    hasBegunBouncing = false; //keeps this block of code from running repeatedly
 
     int lastColorIndex = curColorIndex - 1;
     if (curColorIndex == -1){
